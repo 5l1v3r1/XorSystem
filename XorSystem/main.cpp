ab38f034519184110f351eb37ed4193b6ef5ec2c
Alex Nichol <alexnichol@comcast.net>
2013-10-14 00:48:54 -0400

    Initial Commit

---
+++ XorSystem/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  XorSystem
+//
+//  Created by Alex Nichol on 10/14/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
